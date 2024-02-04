c5f9bf2e6fa192d608af739a7da26bdd0cd2bebf
Sid-26 <siddhantdas262@gmail.com>
2024-02-03 21:46:40 -0500

    im losing my mind

--- serverless/function_app.py
+++ serverless/function_app.py
@@ -66,3 +66,25 @@ def FileUploadToBlob(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(body=json.dumps({"fileUploaded":False}), status_code=400)
 
     return func.HttpResponse(body=json.dumps({"fileUploaded":True}), status_code=201)
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello")
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+     logging.info('Python HTTP trigger function processed a request.')
+
+     name = req.params.get('name')
+     if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+     if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+     else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
